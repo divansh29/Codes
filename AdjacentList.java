8ce05af41cac7959171512ed60d4632946c6b18c
Divansh kothari <113992223+divansh29@users.noreply.github.com>
2025-07-13 12:08:49 +0530

    Create AdjacentList.java

---
+++ AdjacentList.java
@@ -0,0 +1,38 @@
+//Graph creation full 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+      int v=sc.nextInt();
+      int e=sc.nextInt();
+      for(int i=0;i<v;i++){
+        graph.add(new ArrayList<>());
+      }
+      for(int i=0;i<e;i++){
+        int s=sc.nextInt();
+        int d=sc.nextInt();
+        
+        graph.get(s).add(d);
+        graph.get(d).add(s);
+      }
+      boolean[] vis=new boolean[v];
+      for(int i=0;i<v;i++){
+        if(vis[i]==false){
+          dfs(graph,vis,0,v,e);
+        }
+      }
+    }
+  public static void dfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis,int s,int v,int e){
+    vis[s]=true;
+    System.out.print(s+" ");
+    for(int i=0;i<graph.get(s).size();i++){
+      if(vis[graph.get(s).get(i)]==false){
+dfs(graph,vis,graph.get(s).get(i),v,e):
+  }
+    }
+  }
+}
+          
+    
